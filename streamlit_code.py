04289b3f12057e779b96e50c2907992799894a97
TheSignItProject <132065204+TheSignItProject@users.noreply.github.com>
2023-04-28 15:12:51 +0300

    Add files via upload

---
+++ streamlit_code.py
@@ -0,0 +1,39 @@
+import os
+import streamlit as st
+import glob
+#from typing_extensions import TypeGuard
+
+st.title("Audio Examples of paper: Speech2Sing: A Zero-Shot Many-to-Many Speech to Sing Neural Network")
+# st.set_page_config(layout="wide")
+
+
+files_dir = '/sample_audio/sample_audio/full_flow/'
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    #files_dir = '/dsi/gannot-lab/sample_audio/sample_audio/full_flow/'
+    mix_file = os.path.join(files_dir, 'person_a-original_halleluya-acapella.wav')
+    st.text('output1 signal:')
+    st.audio(mix_file)
+    #"/dsi/gannot-lab/sample_audio/sample_audio/full_flow/person_a-original_halleluya-acapella.wav"
+
+with col2:
+    #files_dir = '/dsi/gannot-lab/sample_audio/sample_audio/full_flow/'
+    mix_file = os.path.join(files_dir, 'person_b-m2_s5_speech.wav')
+    st.text('output2 signal:')
+    st.audio(mix_file)
+    #"/dsi/gannot-lab/sample_audio/sample_audio/full_flow/person_b-m2_s5_speech.wav"
+
+with col3:
+    #files_dir = '/dsi/gannot-lab/sample_audio/sample_audio/full_flow/'
+    mix_file = os.path.join(files_dir, 'output-combined_acapella.wav')
+    st.text('output3 signal:')
+    st.audio(mix_file)
+    #"/dsi/gannot-lab/sample_audio/sample_audio/full_flow/output-combined_acapella.wav"
+
+with col4:
+    #files_dir = '/dsi/gannot-lab/sample_audio/sample_audio/full_flow/'
+    mix_file = os.path.join(files_dir, 'output-combined_with_accompany.wav')
+    st.text('output4 signal:')
+    st.audio(mix_file)
+    #"/dsi/gannot-lab/sample_audio/sample_audio/full_flow/output-combined_with_accompany.wav"
+

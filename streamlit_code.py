642f58973c9de10ecdbf505ae1e678c73df67cc0
TheSignItProject <132065204+TheSignItProject@users.noreply.github.com>
2023-04-28 15:20:56 +0300

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -11,21 +11,24 @@ files_dir = '/sample_audio/sample_audio/full_flow/'
 col1, col2, col3, col4 = st.columns(4)
 with col1:
     #files_dir = '/dsi/gannot-lab/sample_audio/sample_audio/full_flow/'
-    mix_file = os.path.join(files_dir, 'person_a-original_halleluya-acapella.wav')
+    #mix_file = os.path.join(files_dir, 'person_a-original_halleluya-acapella.wav')
+    mix_file = 'person_b-m2_s5_speech.wav'
     st.text('output1 signal:')
     st.audio(mix_file)
     #"/dsi/gannot-lab/sample_audio/sample_audio/full_flow/person_a-original_halleluya-acapella.wav"
 
 with col2:
     #files_dir = '/dsi/gannot-lab/sample_audio/sample_audio/full_flow/'
-    mix_file = os.path.join(files_dir, 'person_b-m2_s5_speech.wav')
+    #mix_file = os.path.join(files_dir, 'person_b-m2_s5_speech.wav')
+    mix_file = 'person_b-m2_s5_speech.wav'
     st.text('output2 signal:')
     st.audio(mix_file)
     #"/dsi/gannot-lab/sample_audio/sample_audio/full_flow/person_b-m2_s5_speech.wav"
 
 with col3:
     #files_dir = '/dsi/gannot-lab/sample_audio/sample_audio/full_flow/'
-    mix_file = os.path.join(files_dir, 'output-combined_acapella.wav')
+    #mix_file = os.path.join(files_dir, 'output-combined_acapella.wav')
+    mix_file = 'person_b-m2_s5_speech.wav'
     st.text('output3 signal:')
     st.audio(mix_file)
     #"/dsi/gannot-lab/sample_audio/sample_audio/full_flow/output-combined_acapella.wav"

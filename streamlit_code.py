0801f94c632bea3cc3d0b05c0ab480f1cf7db260
TheSignItProject <132065204+TheSignItProject@users.noreply.github.com>
2023-05-01 16:05:15 +0300

    Update streamlit_code.py
    change text

--- streamlit_code.py
+++ streamlit_code.py
@@ -43,7 +43,7 @@ for i in range(5):
     cols[0].write(row_names[i])
     for j in range(4):
         cols[j+1].audio(dir_path+wav_df[i][j])
-st.write("Person B is 'Male 2 Song 5 Speech' from the 'NHSS' database. Separation of the original audio was done by the 'Spleeter' tool")
+st.write("Person B is 'Male 2 Song 5 - Speech' from the 'NHSS' database. Separation of the original audio was done by the 'Spleeter' tool")
 
 # Song 2
 st.subheader('Song 2 - "Hey Jude"')
@@ -66,7 +66,7 @@ for i in range(5):
     cols[0].write(row_names[i])
     for j in range(4):
         cols[j+1].audio(dir_path+wav_df[i][j])
-st.write("Person B is 'Female 1 Song 7 Speech', and Person A is 'Male 2 Song 5 Song' are both from the 'NHSS' database.")
+st.write("Person B is 'Female 1 Song 7 - Speech', and Person A is 'Male 2 Song 5 - Song' are both from the 'NHSS' database.")
 st.write("The backing track was taken from online source, and not done by the 'Spleeter' tool.")
 
 # Song 3
